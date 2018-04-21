ede2ec71cbddb245e991f1b45935045a710dac8f
Brady Holt <brady.holt@gmail.com>
2018-04-21 17:05:40 -0500

    Rename dist/ to docs/

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ const config = {
     main: [path.join(srcDir, "boot.ts")]
   },
   output: {
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, "docs"),
     filename: "[name].js",
     publicPath: "/"
   },
